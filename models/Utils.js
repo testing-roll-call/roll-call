e7d48b0fb0c8df634da7971a8bb3fe7890c8291a
Marianna <mana998@azet.sk>
2022-04-17 12:02:36 +0200

    Added generate alphanumeric code function

--- models/Utils.js
+++ models/Utils.js
@@ -1,4 +1,12 @@
 class Utils {
+    generateCode(length) {
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        let code = '';
+        for (let i = 0; i < length; i += 1) {
+          code += characters[this.generateRandomNumber(0, characters.length-1)];
+        }
+        return code;
+    }
 
     //max not included
     generateRandomNumber(min, max) {
