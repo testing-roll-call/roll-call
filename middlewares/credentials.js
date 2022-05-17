fd9b450797b48f355daf8a1f183866374e5f4c3a
Gabriel Nicutari <gabriel_nicutari@yahoo.ro>
2022-05-17 09:18:04 +0200

    Updated requireAuth middleware & Created crendials middleware (http-only cookie)

---
+++ middlewares/credentials.js
@@ -0,0 +1,12 @@
+const allowedOrigins = ['http://localhost:3000'];
+
+const credentials = (req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    console.log('YES');
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+  next();
+};
+
+module.exports = credentials;
