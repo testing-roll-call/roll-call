c97717978d27436932a2a674686b629295d16903
Marianna <mana998@azet.sk>
2022-05-20 20:19:53 +0200

    Updated functions based on tests

--- models/Utils.js
+++ models/Utils.js
@@ -1,7 +1,14 @@
 class Utils {
     generateCode(length) {
-        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        if (typeof (length) !== 'number') {
+            throw('Passed parameter is not a number.');
+        }
         let code = '';
+        //length limit to 100 else empty string
+        if (length > 100) {
+            return code;
+        }
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
         for (let i = 0; i < length; i += 1) {
           code += characters[this.generateRandomNumber(0, characters.length-1)];
         }
@@ -10,7 +17,17 @@ class Utils {
 
     //max not included
     //return type number test, integer and not decimal
-    generateRandomNumber(min, max) {
+    generateRandomNumber(min = 0, max = 1) {
+        if (typeof (min) !== 'number' || typeof (max) !== 'number') throw new Error('Passed parameter is not a number.');
+        //swap - could be extracted into its own method if it is needed at multiple places
+        if (min > max) {
+            let temp = max;
+            max = min;
+            min = temp;
+        }
+        if (min < 0 || max < 0) {
+            return 0;
+        }
         return Math.floor(Math.random() * (max - min)) + min;
     }
 }
