7dcfc2fda835171bb3fab41e9e9300a28acb7df7
Gabriel <gabriel_nicutari@yahoo.ro>
2022-05-28 16:29:38 +0200

    Updated Config to support 2 db schemas

--- database/connection.js
+++ database/connection.js
@@ -3,12 +3,15 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 // const cert = fs.readFileSync('database/BaltimoreCyberTrustRoot.crt.pem');
+const test = process.env.ENV;
+console.log(test);
 
 const config = {
   host: process.env.AZURE_HOST,
   user: process.env.AZURE_USER,
   password: process.env.AZURE_PASSWORD,
-  database: process.env.AZURE_DATABASE,
+  database:
+    test === 'test' ? process.env.AZURE_TEST_DATABASE : process.env.AZURE_DATABASE,
   port: 3306,
   connectionLimit: 10
   // ssl: { ca: cert }
