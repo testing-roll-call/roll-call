1def3dffbcb496553a9dfb6a416be4630ec8980c
Gabriel <gabriel_nicutari@yahoo.ro>
2022-05-28 15:42:10 +0200

    Azure DB Config

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,21 @@
 const mysql = require('mysql2');
+// const fs = require('fs');
 require('dotenv').config();
 
-const pool = mysql.createPool(process.env.CLEARDB_DATABASE_URL, {
-  connectionLimit: 5,
-});
+// const cert = fs.readFileSync('database/BaltimoreCyberTrustRoot.crt.pem');
+
+const config = {
+  host: process.env.AZURE_HOST,
+  user: process.env.AZURE_USER,
+  password: process.env.AZURE_PASSWORD,
+  database: process.env.AZURE_DATABASE,
+  port: 3306,
+  connectionLimit: 10
+  // ssl: { ca: cert }
+};
+
+const pool = mysql.createPool(config);
 
 module.exports = {
-  pool,
+  pool
 };
